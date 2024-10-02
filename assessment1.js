ce2eba305b1b723748cd72887346fd706b24f420
Ashish Mishra <ash.ishu025@gmail.com>
2024-10-02 20:55:10 +0530

    Assessment 1

--- assessment1.js
+++ assessment1.js
@@ -1,5 +1,6 @@
-//March - MERNStack Session - Assessment Number 1 - 2nd October 2024
-//Q1. Create a file with name basics and show all the features that you know about javascript?
+//October - MERNStack Session - Assessment Number 1 - 2nd October 2024
+
+//Q1. Create a file with name basics and show all the features that you know about javascript? (minimum 5 and maximum 8 topics)
 // Try explaining in 1-2 lines : example - Prototype : An object which behaves as a link between two functions and provides inheritance
 
 //Q2. As javascript is not a type safe and has auto cast feature - try showing below values from the same variable
@@ -9,7 +10,7 @@
 //  print all the details in the given function
 
 //Q4. Create a function with name doaddition, pass three parameters and return the sum of all the three numbers
-// below output needs to be monitored - add(2,3,4), add(2), add(2.3,3), add("first", 2, "three")
+// below output needs to be monitored - doaddition(2,3,4), doaddition(2), doaddition(2.3,3), doaddition("first", 2, "three")
 // analyse the outputs we get and try explaining the reasons behind!!
 
 //Q5. Give me an example of your choice for each of the below concepts
@@ -21,8 +22,8 @@
 
 //Q7. Create an example of bind using Student object, where a function returns data with SetTimeOut and we fix it by bind.
 
-//Q8. Create an example of creating object with null prototype
+//Q8. Create an example of creating object with null prototype. What would be the purpose of the same?
 
 //Q9. How do we merge different objects properties using Object class function
 
-//Q10. Create an object literal and export it to another file and import and show that there
+//Q10. Create an object literal and export it to another file and import and show that there, by logging the value returned
