1faec9daa8ff71cf0c080dd9ce50181b5b0a8abd
Ashish Mishra <ash.ishu025@gmail.com>
2024-10-02 20:42:06 +0530

    Assessment 1

--- Test.js
+++ Test.js
@@ -53,3 +53,57 @@ console.log(sumValue) //
 // Hoisting
 // Datatypes
 // Modules
+
+// Context - 
+
+// class Calculator { 
+//     //<this>
+
+//     const pi = 3.14123;
+//     const margins = +5,-5;
+//     const userList = require("./user.json")
+
+//     public List UserDetails(){
+//         return this.userList
+//     }
+//     public Addition(a,b){
+//     }
+//     public Substraction(c,d){
+//     }
+// }
+
+// var ObjCalc = new Calculator()
+// ObjCalc.Substraction();
+
+//function global() { //in the given module
+    
+pi = 3.141
+
+function UserDetails(){
+    return this.userList
+}
+
+function Addition(margin, n_margin){    
+    //return a+b+pi+e;
+
+    return this.firstValue + this.secondValue + margin - n_margin + pi
+}
+
+function Substraction(c,d){
+    return c+d;
+}
+
+Addition(5,6)
+
+var CalculateValue = {firstValue : 5, secondValue : 6}
+
+Addition.call(CalculateValue,0,0) //set the context of addition function to - CalculateValue
+
+//}
+
+//context - refers to the accesibility of the variables properties functions callbacks in the function which it can use at the time of execution
+// by default - global (module)/windows are the context of any js functions
+// context in js is refered with the keyword - this and it is highly dynamic in nature
+// execution context - the context we change to get the desire behaviour from the funtion and is the most updated value of the context
+
+// context, execution context and scope are used interchangeably and referred with the keyword - this
---
+++ assessment1.js
@@ -0,0 +1,28 @@
+//March - MERNStack Session - Assessment Number 1 - 2nd October 2024
+//Q1. Create a file with name basics and show all the features that you know about javascript?
+// Try explaining in 1-2 lines : example - Prototype : An object which behaves as a link between two functions and provides inheritance
+
+//Q2. As javascript is not a type safe and has auto cast feature - try showing below values from the same variable
+// and its type as well :values are - "Robert ", .0266, false, {myname : "Test Me"}, 25166665, undefined, true, "Robert Jr.", null, {}, -32767
+
+//Q3. Create a function with name showUserInfo, this function expects three params, firstname, lastname and age
+//  print all the details in the given function
+
+//Q4. Create a function with name doaddition, pass three parameters and return the sum of all the three numbers
+// below output needs to be monitored - add(2,3,4), add(2), add(2.3,3), add("first", 2, "three")
+// analyse the outputs we get and try explaining the reasons behind!!
+
+//Q5. Give me an example of your choice for each of the below concepts
+// a. closure, 
+// b. hoisting, 
+// c. constructor function
+
+//Q6. What is the purpose of call, apply and bind ? and why they are used ? whats the difference between bind and apply ?
+
+//Q7. Create an example of bind using Student object, where a function returns data with SetTimeOut and we fix it by bind.
+
+//Q8. Create an example of creating object with null prototype
+
+//Q9. How do we merge different objects properties using Object class function
+
+//Q10. Create an object literal and export it to another file and import and show that there
