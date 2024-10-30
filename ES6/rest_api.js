262d8feeb0a5faba9956b4730f9debb44c818be6
Ashish Mishra <ash.ishu025@gmail.com>
2024-10-30 21:05:13 +0530

    node systems

--- ES6/rest_api.js
+++ ES6/rest_api.js
@@ -4,3 +4,29 @@
 //http -> stateless 
 
 //state - 
+
+//Service Oriented Architecture - SOA
+//Implements - Separation of Concerns 
+//<backend (Service, API, Endpoints)>
+//<frontend (html, css, js, and its frameworks - angular, react, ...etc with SPA)>
+
+// Restful API - Standards or Specifications
+
+// TCP/IP and WCF or IMAP etc - HTTP => all rest api's must have HTTP/s protocol
+
+// It should follow http verbs or CRUD operations
+
+// Post =>  Save/Create data in server (Create)
+// Get/GetAll => Fetch data from server (Read)
+// Put/Patch => update data to server (Update)
+// Delete => delete data on the basis of id passed (Delete)
+
+// Put - updates entire record (each field and the value) + id
+// Patch - updates specific records (not each field) + id
+
+
+// www.learnmern.edu/api/user 
+// Get/Get by ID
+// Post - data in payload
+// Put/Patch - data in payload
+// Delete - with id
