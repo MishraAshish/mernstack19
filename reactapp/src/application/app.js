5e9355662d87d2d2c3c71599711759c79324957b
Ashish Mishra <ash.ishu025@gmail.com>
2024-11-07 22:18:46 +0530

    react app setup working

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -4,8 +4,9 @@ export default class Application extends React.Component {
 
 
     render(){
+        return(
         <>
             <h2>This is my first react page from application </h2>
-        </>
+        </>)
     }
 }
