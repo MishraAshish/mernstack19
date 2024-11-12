7f148fbc7a8996deb2174d1c732f24fdefb8032a
Ashish Mishra <ash.ishu025@gmail.com>
2024-11-12 22:09:06 +0530

    react code with state change and virtual dom

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 export default class Application extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            userName : "Duncan"
+        }
+    }
+
+    onclick = (evt)=>{
+        console.log("Name change click is clicked")
+        this.setState({
+            userName : "Sierra"
+        })
+
+        evt.preventDefault();
+    }
+
+    //render - method is responsible to create virtual dom for every change of state or props
     render(){
         let myname = "Windiee - JSX => Javascript Like XML Structure"
         let nameList = ["Nilay", "Gesan", "Jimmy", "Ben Ma", "Jay", "Everyone else"]
@@ -16,6 +34,9 @@ export default class Application extends React.Component {
             <h3>Still Working</h3>
             {nameListWorking && nameListWorking.map((name)=>{
                 return <b><hr/>{name}</b>})}
+            <hr/>
+            <h2>{this.state.userName}</h2>
+            <button onClick={this.onclick}> Change Name</button>
         </>)
     }
 }
