59a5bbef703cdaf341c671ccade19df378a32d94
Ashish Mishra <ash.ishu025@gmail.com>
2025-01-08 20:53:27 +0530

    Checkout component

--- reactapp/src/application/CommonComponents/ProjectEnhancement.js
+++ reactapp/src/application/CommonComponents/ProjectEnhancement.js
@@ -1,3 +1,19 @@
+// 08-Jan-2025 - Checkout Component
+
+// Checkout Component
+// Create A functional component and use react hook or using container to read data from store
+// Show - User Details (Name, address) //We will deliver products to below address kind of message as well
+// Show Table of cart put up for purchase (you need to re-use the cartlist and cartitem components)
+// Show the purchase Summary (total qty and total amount)
+// Show a Button to Proceed to Payment
+// Integrate this page on CartComponent Button (Go to checkout) -(Go To Checkout From Cart Component)
+
+// Second Task :
+// Create a state using useState to show hide (Make Payment Message)
+// Upon Clicking on MakePayment button, hide everything and just show the message - "Thankyou for the payment, your items under process!"
+// Change the header from Checkout Page to Payment Page
+
+
 // 20-Dec-2024 - Cart Implementation
 
 // Cart Implementation
