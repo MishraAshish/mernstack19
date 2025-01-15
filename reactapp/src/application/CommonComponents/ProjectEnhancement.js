219468abbb8120fb5ccf7e86b9a035096e17140b
Ashish Mishra <ash.ishu025@gmail.com>
2025-01-15 21:08:31 +0530

    recent order

--- reactapp/src/application/CommonComponents/ProjectEnhancement.js
+++ reactapp/src/application/CommonComponents/ProjectEnhancement.js
@@ -1,3 +1,12 @@
+// 15-Jan-2025 - Recentorder Page 
+// Upon MakePayment Click
+// Save the cart to RecentOrders collection (should have userid, order, dateTime)
+// Make API to Save and Fetch from RecentOrders
+// Make a component RecentOrders to Show all previous Orders of current user
+// Add a button to Cancel (like) we have remove in CartComponent and then save again, 
+// order can be cancelled within 2 days after that it should be marked delivered
+
+
 // 13-Jan-2025 - Coupon Page
 
 // Coupon Page 
