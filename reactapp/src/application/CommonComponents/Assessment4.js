a10270c68e39ee5d0bd790fde4e21b2f58735420
Ashish Mishra <ash.ishu025@gmail.com>
2025-01-28 21:17:59 +0530

    assessment 4

---
+++ reactapp/src/application/CommonComponents/Assessment4.js
@@ -0,0 +1,27 @@
+//Assessment #4 - 28th Jan 2025
+
+// Q1. Create a page to show time, in HH:MM:SS, it should be updated each second 
+
+// Q2. Create a component ATM Dispencer, IT will contain a text box to enter amount that needs to be withdrawn
+// // a button - with text withdraw : upon click it should display the minimum set of denominations of notes
+// 	Create a react page like atm dispenser using hooks, it should update the denomination (Currency Notes) within functional component or
+// 	create this using react class component
+// 	Denominations are of : 1,2,5,10,20,50,100,200,500,1000,2000
+// 	E.G. Amount 7863 -
+// 	You will get following amount
+// 	1 notes of Rs 1
+// 	1 notes of Rs 2
+// 	0 notes of Rs 5
+// 	1 notes of Rs 10
+// 	0 notes of Rs 20
+// 	1 notes of Rs 50
+// 	1 notes of Rs 100
+// 	1 notes of Rs 200
+// 	3 notes of Rs 500
+// 	3 notes of Rs 2000
+// 	Total notes dispensed: 12
+
+// Q3. Create a page to save hobby, fetch the list of hobbies, in user or signin component, show them in drop down.
+// 	Add a button on sign/up page to update userinfo, once clickd it should save the hobby to user's document
+	
+// Q4. Your work till checkout is done - Yes/No
